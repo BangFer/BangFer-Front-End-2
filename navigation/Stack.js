4c48789f502139e87092cb750de480d5b2392a9e
Ohwooseok <63221878+Ohwooseok@users.noreply.github.com>
2024-05-23 16:21:01 +0900

    ✨ feat: Plus, Enroll, BanggusukTeam 페이지 세부 개발

--- navigation/Stack.js
+++ navigation/Stack.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native-stack';
 import { FontAwesome } from '@expo/vector-icons';
@@ -21,6 +21,9 @@ import PostExample from '../screens/PostExample';
 import Login from '../screens/Login';
 import SignUp from '../screens/SignUp';
 import FindPw from '../screens/FindPw';
+import EnrollBanggusukTeam from '../screens/EnrollBanggusukTeam';
+import { Calendar, LocaleConfig } from 'react-native-calendars';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 import styled from 'styled-components';
 import {
@@ -30,6 +33,7 @@ import {
   Image,
   TouchableOpacity,
   Button,
+  Modal,
 } from 'react-native';
 
 // your entry point
@@ -49,7 +53,69 @@ import {
   MenuTrigger,
 } from 'react-native-popup-menu';
 
+LocaleConfig.locales['fr'] = {
+  monthNames: [
+    '01월',
+    '02월',
+    '03월',
+    '04월',
+    '05월',
+    '06월',
+    '07월',
+    '08월',
+    '09월',
+    '10월',
+    '11월',
+    '12월',
+  ],
+  monthNamesShort: [
+    '01월',
+    '02월',
+    '03월',
+    '04월',
+    '05월',
+    '06월',
+    '07월',
+    '08월',
+    '09월',
+    '10월',
+    '11월',
+    '12월',
+  ],
+  dayNames: [
+    '일요일',
+    '월요일',
+    '화요일',
+    '수요일',
+    '목요일',
+    '금요일',
+    '토요일',
+  ],
+  dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+  today: "Aujourd'hui",
+};
+LocaleConfig.defaultLocale = 'fr';
+
 const Stack = createNativeStackNavigator();
+
+const TouchCalander = styled.TouchableOpacity`
+  width: 32px;
+  height: 32px;
+  align-items: center;
+  justify-content: center;
+  margin-right: 5px;
+`;
+
+const CalanderEnrollButton = styled.TouchableOpacity`
+  width: 70px;
+  height: 30px;
+  border-radius: 8px;
+  background-color: red;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
+  margin-left: 240px;
+`;
 
 const SearchButton = styled.TouchableOpacity`
   margin-right: 10px;
@@ -74,14 +140,41 @@ const navigateToMyTactics = (navigation) => {
 };
 
 const navigateToMyPosts = (navigation) => {
-  navigation.navigate("MyPosts");
+  navigation.navigate('MyPosts');
 };
 
 const navigateToNewPost = (navigation) => {
-  navigation.navigate("NewPost");
+  navigation.navigate('NewPost');
 };
 
 const StackNavigation = () => {
+  const [isCallendarVisible, setIsCallendarVisible] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [markedDates, setMarkedDates] = useState({});
+  const [matchRegistered, setMatchRegistered] = useState(false); // 1. 매치 등록 상태 추가
+  const markSelectedDate = () => {
+    if (selectedDate) {
+      if (markedDates[selectedDate]) {
+        // 이미 선택된 날짜를 클릭한 경우
+        setMarkedDates((prevMarkedDates) => {
+          const updatedMarkedDates = { ...prevMarkedDates };
+          delete updatedMarkedDates[selectedDate]; // 선택된 날짜의 마킹을 제거
+          return updatedMarkedDates;
+        });
+        setMatchRegistered(false); // 매치 취소 상태로 변경
+      } else {
+        setMarkedDates({
+          ...markedDates,
+          [selectedDate]: {
+            selected: true,
+            selectedColor: 'red',
+          },
+        });
+        setMatchRegistered(true);
+      }
+    }
+  };
+
   return (
     <MenuProvider>
       <Stack.Navigator
@@ -115,91 +208,223 @@ const StackNavigation = () => {
                   <Menu>
                     <MenuTrigger>
                       <Feather name="more-vertical" size={24} color="black" />
-                      </MenuTrigger>
-                      <MenuOptions>
-                      <MenuOption onSelect={() => navigateToNewTactic(navigation)} text='새 전술 생성' />
-                      <MenuOption onSelect={() => navigateToMyTactics(navigation)}>
-                        <Text style={{color: 'red'}}>내 전술 보기</Text>
-                        </MenuOption>
-                      </MenuOptions>
-                    </Menu>
-                    </NavigationButtonView>
-                  </View>
-                  
-                    ),
-                  })}
-                />
+                    </MenuTrigger>
+                    <MenuOptions>
+                      <MenuOption
+                        onSelect={() => navigateToNewTactic(navigation)}
+                        text="새 전술 생성"
+                      />
+                      <MenuOption
+                        onSelect={() => navigateToMyTactics(navigation)}
+                      >
+                        <Text style={{ color: 'red' }}>내 전술 보기</Text>
+                      </MenuOption>
+                    </MenuOptions>
+                  </Menu>
+                </NavigationButtonView>
+              </View>
+            ),
+          })}
+        />
 
-      <Stack.Screen name ='TacticsSearch' component={TacticsSearch} options={{ headerShown: false ,headerTitleAlign: 'center'}}/>
-      <Stack.Screen name='NewTactic' component={NewTactic} options={{ headerShown: true ,headerTitleAlign: 'center'}}/>
-      <Stack.Screen name='MyTactics' component={MyTactics} options={{ headerShown: true ,headerTitleAlign: 'center'}}/>
-      <Stack.Screen name='TacticExample' component={TacticExample} options={{ headerShown: true ,headerTitleAlign: 'center'}}/>
+        <Stack.Screen
+          name="TacticsSearch"
+          component={TacticsSearch}
+          options={{ headerShown: false, headerTitleAlign: 'center' }}
+        />
+        <Stack.Screen
+          name="NewTactic"
+          component={NewTactic}
+          options={{ headerShown: true, headerTitleAlign: 'center' }}
+        />
+        <Stack.Screen
+          name="MyTactics"
+          component={MyTactics}
+          options={{ headerShown: true, headerTitleAlign: 'center' }}
+        />
+        <Stack.Screen
+          name="TacticExample"
+          component={TacticExample}
+          options={{ headerShown: true, headerTitleAlign: 'center' }}
+        />
 
+        <Stack.Screen
+          name="PlusBanggusukTeam"
+          component={PlusBanggusukTeam}
+          options={({ navigation }) => ({
+            title: '새 팀',
+            headerTitleAlign: 'center',
+            headerShown: true,
 
-      <Stack.Screen
-        name="PlusBanggusukTeam"
-        component={PlusBanggusukTeam}
-        options={({ navigation }) => ({
-          title: '새 팀',
-          headerTitleAlign: 'center',
-        })}
-      />
+            headerRight: () => (
+              <TouchCalander onPress={() => setIsCallendarVisible(true)}>
+                <AntDesign name="calendar" size={32} color="black" />
+              </TouchCalander>
+            ),
+          })}
+        />
+        <Stack.Screen
+          name="EnrollBanggusukTeam"
+          component={EnrollBanggusukTeam}
+          options={() => ({
+            title: '방구석 팀 등록',
+            headerTitleAlign: 'center',
+            headerShown: true,
+          })}
+        />
+        <Stack.Screen
+          name="BanggusukTeam"
+          component={BanggusukTeam}
+          options={({ navigation }) => ({
+            title: '방구석 팀',
+            headerShown: true,
 
-      <Stack.Screen
-        name="BanggusukTeam"
-        component={BanggusukTeam}
-        options={({ navigation }) => ({
-          title: '방구석 팀',
-          headerTitleAlign: 'center',
-          headerRight: () => (
-            <TouchableOpacity
-              onPress={() => navigation.navigate('PlusBanggusukTeam')}
-            >
-              <Text style={{ fontSize: 25 }}>+</Text>
-            </TouchableOpacity>
-          ),
-        })}
-      />
+            headerTitleAlign: 'center',
+            headerRight: () => (
+              <TouchableOpacity
+                onPress={() => navigation.navigate('EnrollBanggusukTeam')}
+              >
+                <Text style={{ fontSize: 25 }}>+</Text>
+              </TouchableOpacity>
+            ),
+          })}
+        />
 
-      <Stack.Screen 
-      name='FreeBoard' 
-      component={FreeBoard} 
-      options={({ navigation }) => ({
-                headerShown: true ,
-                headerTitleAlign: 'center',
-                headerRight: () => (
-                  <View>
-                    <NavigationButtonView>
-                      <SearchButton onPress={() => navigation.navigate("FreeBoardSearch")}>
-                        <FontAwesome name="search" size={20} color="black" />
-                      </SearchButton>
-                      <Menu>
-                      <MenuTrigger>
+        <Stack.Screen
+          name="FreeBoard"
+          component={FreeBoard}
+          options={({ navigation }) => ({
+            headerShown: true,
+            headerTitleAlign: 'center',
+            headerRight: () => (
+              <View>
+                <NavigationButtonView>
+                  <SearchButton
+                    onPress={() => navigation.navigate('FreeBoardSearch')}
+                  >
+                    <FontAwesome name="search" size={20} color="black" />
+                  </SearchButton>
+                  <Menu>
+                    <MenuTrigger>
                       <Feather name="more-vertical" size={24} color="black" />
-                      </MenuTrigger>
-                      <MenuOptions>
-                      <MenuOption onSelect={() => navigateToNewPost(navigation)} text='글 쓰기' />
-                      <MenuOption onSelect={() => navigateToMyPosts(navigation)}>
-                        <Text style={{color: 'red'}}>내가 쓴 글</Text>
-                        </MenuOption>
-                      </MenuOptions>
-                    </Menu>
-                    </NavigationButtonView>
-                  </View>
-                  
-                    ),
-                  })}
-                />
-      <Stack.Screen name ='FreeBoardSearch' component={FreeBoardSearch} options={{ headerShown: false ,headerTitleAlign: 'center'}}/>
-      <Stack.Screen name ='MyPosts' component={MyPosts} options={{ headerShown: true ,headerTitleAlign: 'center'}}/>
-      <Stack.Screen name ='NewPost' component={NewPost} options={{ headerShown: true ,headerTitleAlign: 'center'}}/>
+                    </MenuTrigger>
+                    <MenuOptions>
+                      <MenuOption
+                        onSelect={() => navigateToNewPost(navigation)}
+                        text="글 쓰기"
+                      />
+                      <MenuOption
+                        onSelect={() => navigateToMyPosts(navigation)}
+                      >
+                        <Text style={{ color: 'red' }}>내가 쓴 글</Text>
+                      </MenuOption>
+                    </MenuOptions>
+                  </Menu>
+                </NavigationButtonView>
+              </View>
+            ),
+          })}
+        />
+        <Stack.Screen
+          name="FreeBoardSearch"
+          component={FreeBoardSearch}
+          options={{ headerShown: false, headerTitleAlign: 'center' }}
+        />
+        <Stack.Screen
+          name="MyPosts"
+          component={MyPosts}
+          options={{ headerShown: true, headerTitleAlign: 'center' }}
+        />
+        <Stack.Screen
+          name="NewPost"
+          component={NewPost}
+          options={{ headerShown: true, headerTitleAlign: 'center' }}
+        />
 
-      <Stack.Screen name='MyPage' component={MyPage} options={{ headerShown: false ,headerTitleAlign: 'center'}}/>
-      <Stack.Screen name='PostExample' component={PostExample} options={{ headerShown: false ,headerTitleAlign: 'center'}}/>
-
-    </Stack.Navigator>
-  </MenuProvider>
-);
+        <Stack.Screen
+          name="MyPage"
+          component={MyPage}
+          options={{ headerShown: false, headerTitleAlign: 'center' }}
+        />
+        <Stack.Screen
+          name="PostExample"
+          component={PostExample}
+          options={{ headerShown: false, headerTitleAlign: 'center' }}
+        />
+      </Stack.Navigator>
+      {isCallendarVisible && ( // isCallendarVisible 상태가 true일 때 모달이 보이도록 설정합니다.
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={isCallendarVisible}
+        >
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              backgroundColor: 'rgba(0, 0, 0, 0.5)',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            onPress={() => setIsCallendarVisible(false)}
+          >
+            <View
+              style={{
+                flex: 0.46,
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRadius: 8,
+                borderWidth: 4,
+                backgroundColor: 'white',
+              }}
+            >
+              <Calendar
+                style={{ borderRadius: 8, width: 350, hight: 80 }}
+                current={'2024-05-01'}
+                markedDates={{
+                  ...markedDates,
+                  [selectedDate]: {
+                    selected: true,
+                    selectedColor: 'red',
+                  },
+                }}
+                onDayPress={(day) => {
+                  console.log('선택된 날', day);
+                  setSelectedDate(day.dateString);
+                }}
+                monthFormat={'yyyy.MM'}
+                hideExtraDays={true}
+                firstDay={1}
+                theme={{
+                  'stylesheet.calendar.main': {
+                    selectedDay: {
+                      backgroundColor: 'red',
+                    },
+                  },
+                  'stylesheet.calendar.header': {
+                    dayTextAtIndex0: {
+                      color: '#FF0000',
+                    },
+                    dayTextAtIndex6: {
+                      color: '#007BA4',
+                    },
+                  },
+                  backgroundColor: '#ffffff',
+                  arrowColor: '#5B5B5B',
+                }}
+              ></Calendar>
+              <CalanderEnrollButton onPress={markSelectedDate}>
+                <Text
+                  style={{ color: 'white', fontSize: 14, fontWeight: 'bold' }}
+                >
+                  {markedDates[selectedDate] ? '매치 취소' : '매치 등록'}
+                </Text>
+              </CalanderEnrollButton>
+            </View>
+          </TouchableOpacity>
+        </Modal>
+      )}
+    </MenuProvider>
+  );
 };
 
-export default StackNavigation
+export default StackNavigation;
--- screens/BanggusukTeam.js
+++ screens/BanggusukTeam.js
@@ -1,11 +1,249 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { Text, View, FlatList, TouchableOpacity } from "react-native";
+import styled from "styled-components";
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import PlusBanggusukTeam from "../screens/PlusBanggusukTeam";
+import { MenuProvider } from "react-native-popup-menu";
+import {
+  Menu,
+  MenuOptions,
+  MenuOption,
+  MenuTrigger,
+} from "react-native-popup-menu";
+const Container = styled.View`
+  width: 100%;
+  height: 100%;
+`;
+
+const ViewForFlatListTitle = styled.View`
+  width: 100%;
+  height: 40%;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const ViewForFlatListMiddle = styled.View`
+  width: 100%;
+  height: 30%;
+`;
+
+const TouchForFlatList = styled.TouchableOpacity`
+  width: 100%;
+  height: 60px;
+`;
+
+const ViewForFlatListBottom = styled.View`
+  width: 100%;
+  height: 30%;
+  flex-direction: row;
+`;
+
+const TextForFlatListTitle = styled.Text`
+  font-weight: bold;
+  color: black;
+  font-size: 16px;
+`;
+
+const TextForFlatListTaticsName = styled.Text`
+  color: black;
+  font-size: 14px;
+`;
+
+const TextForFlatListBottom = styled.Text`
+  color: grey;
+  font-size: 12px;
+`;
+
+const ViewForSeparator = styled.View`
+  width: 100%;
+  height: 2px;
+  background-color: black;
+`;
+
+const navigateToPlusBanggusukTeam = (navigation) => {
+  navigation.navigate("PlusBanggusukTeam");
+};
+
+const Item = ({
+  title,
+  TaticsName,
+  DirecterName,
+  Formation,
+  Member,
+  navigation,
+}) => {
+  return (
+    <TouchForFlatList onPress={() => navigation.navigate("PlusBanggusukTeam")}>
+      <ViewForFlatListTitle>
+        <TextForFlatListTitle>{title}</TextForFlatListTitle>
+        <Menu>
+          <MenuTrigger>
+            <Feather name="more-vertical" size={20} color="black" />
+          </MenuTrigger>
+          <MenuOptions
+            customStyles={{
+              optionsContainer: { width: 40, height: 60 },
+            }}
+          >
+            <MenuOption
+              onSelect={() => navigateToPlusBanggusukTeam(navigation)}
+            >
+              <Text>수정</Text>
+            </MenuOption>
+            <MenuOption>
+              <Text>삭제</Text>
+            </MenuOption>
+          </MenuOptions>
+        </Menu>
+      </ViewForFlatListTitle>
+      <ViewForFlatListMiddle>
+        <TextForFlatListTaticsName>{TaticsName}</TextForFlatListTaticsName>
+      </ViewForFlatListMiddle>
+      <ViewForFlatListBottom>
+        <TextForFlatListBottom>{DirecterName}</TextForFlatListBottom>
+        <TextForFlatListBottom>|</TextForFlatListBottom>
+        <TextForFlatListBottom>{Formation}</TextForFlatListBottom>
+        <TextForFlatListBottom>|</TextForFlatListBottom>
+        <TextForFlatListBottom>{Member}</TextForFlatListBottom>
+      </ViewForFlatListBottom>
+    </TouchForFlatList>
+  );
+};
+
+const DATA = [
+  {
+    id: "1",
+    title: "스뮤니티",
+    TaticsName: "전술1",
+    DirecterName: "오우석",
+    Formation: "4-4-2",
+    Member: "14명",
+  },
+  {
+    id: "2",
+    title: "스뮤니티",
+    TaticsName: "전술1",
+    DirecterName: "오우석",
+    Formation: "4-4-2",
+    Member: "14명",
+  },
+  {
+    id: "3",
+    title: "스뮤니티",
+    TaticsName: "전술1",
+    DirecterName: "오우석",
+    Formation: "4-4-2",
+    Member: "14명",
+  },
+  {
+    id: "4",
+    title: "스뮤니티",
+    TaticsName: "전술1",
+    DirecterName: "오우석",
+    Formation: "4-4-2",
+    Member: "14명",
+  },
+  {
+    id: "5",
+    title: "스뮤니티",
+    TaticsName: "전술1",
+    DirecterName: "오우석",
+    Formation: "4-4-2",
+    Member: "14명",
+  },
+  {
+    id: "6",
+    title: "스뮤니티",
+    TaticsName: "전술1",
+    DirecterName: "오우석",
+    Formation: "4-4-2",
+    Member: "14명",
+  },
+  {
+    id: "7",
+    title: "스뮤니티",
+    TaticsName: "전술1",
+    DirecterName: "오우석",
+    Formation: "4-4-2",
+    Member: "14명",
+  },
+  {
+    id: "8",
+    title: "스뮤니티",
+    TaticsName: "전술1",
+    DirecterName: "오우석",
+    Formation: "4-4-2",
+    Member: "14명",
+  },
+  {
+    id: "9",
+    title: "스뮤니티",
+    TaticsName: "전술1",
+    DirecterName: "오우석",
+    Formation: "4-4-2",
+    Member: "14명",
+  },
+  {
+    id: "10",
+    title: "스뮤니티",
+    TaticsName: "전술1",
+    DirecterName: "오우석",
+    Formation: "4-4-2",
+    Member: "14명",
+  },
+  {
+    id: "11",
+    title: "스뮤니티",
+    TaticsName: "전술1",
+    DirecterName: "오우석",
+    Formation: "4-4-2",
+    Member: "14명",
+  },
+  {
+    id: "12",
+    title: "스뮤니티",
+    TaticsName: "전술1",
+    DirecterName: "오우석",
+    Formation: "4-4-2",
+    Member: "14명",
+  },
+  {
+    id: "13",
+    title: "스뮤니티",
+    TaticsName: "전술1",
+    DirecterName: "오우석",
+    Formation: "4-4-2",
+    Member: "14명",
+  },
+];
 
 const BanggusukTeam = ({ navigation }) => {
   return (
-    <View>
-      <Text>BanggusukTeam</Text>
-    </View>
+    <Container>
+      <View style={{ width: "100%", height: "100%" }}>
+        <FlatList
+          data={DATA}
+          renderItem={({ item }) => (
+            <Item
+              title={item.title}
+              TaticsName={item.TaticsName}
+              DirecterName={item.DirecterName}
+              Formation={item.Formation}
+              Member={item.Member}
+              navigation={navigation}
+            />
+          )}
+          keyExtractor={(item) => item.id}
+          ItemSeparatorComponent={ViewForSeparator}
+          initialNumToRender={10}
+          nestedScrollEnabled={true}
+          maxToRenderPerBatch={10}
+          removeClippedSubview="true"
+        />
+      </View>
+    </Container>
   );
 };
 
